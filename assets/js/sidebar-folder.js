cefeeba83db40762cd15a9d82496b9d30b0e7bf4
applelime <app.le@kakao.com>
2021-12-26 16:14:46 +0900

    fix: close submenu when click menu or submenu

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -1,3 +1,5 @@
+let currentSpreadSubMenu = 0;
+
 function spread(count) {
     // 마우스 클릭하면 서브메뉴 노출 변경
     let submenu = document.getElementById('submenu-' + count);
@@ -6,15 +8,35 @@ function spread(count) {
     if (submenu) {        
         if (submenu.classList.contains('hide')) {
             submenu.classList.remove('hide');
+            currentSpreadSubMenu = count;
             if (icon) {
                 icon.innerHTML = 'arrow_drop_down';
             }
         }        
         else {
             submenu.classList.add('hide');
+            currentSpreadSubMenu = 0;
             if (icon) {
                 icon.innerHTML = 'arrow_right'
-            }            
+            }
+        }
+    }
+}
+
+function cover() {
+    if (0 == currentSpreadSubMenu)
+    {
+        return;
+    }
+
+    let submenu = document.getElementById('submenu-' + currentSpreadSubMenu);
+    let icon = document.getElementById('spread-icon-' + currentSpreadSubMenu);
+
+    if (submenu) {        
+        submenu.classList.add('hide');
+        currentSpreadSubMenu = 0;
+        if (icon) {
+            icon.innerHTML = 'arrow_right'
         }
     }
 }
