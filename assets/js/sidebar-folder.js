d2e10aac982240e958d98544615062d8aeded31b
applelime <app.le@kakao.com>
2022-10-30 21:25:17 +0900

    fix: 서브메뉴가 열리지 않는 현상 수정

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -9,3 +9,38 @@ function changeColor(count, on) {
         }
     }
 }
+
+
+// 서브메뉴 open/close 처리
+function processSubMenu() {
+document.body.addEventListener("click", (e) => {
+    const buttons = document.querySelectorAll(".spread-btn");
+
+    buttons.forEach((button) => {
+    const icon = button.querySelector("span");
+    const submenuId = `submenu-${icon.id.split("-")[2]}`;
+    const submenu = document.getElementById(submenuId);
+
+    // 화살표 버튼을 눌렀을 때
+    if (button.contains(e.target)) {
+        // 닫혀있으면 (hide 상태라면) 열어준다.
+        if (submenu.classList.contains("hide")) {
+        submenu.classList.remove("hide");
+        if (icon) {
+            icon.innerHTML = "arrow_drop_down";
+        }
+        } else { // 열려있으면 닫는다.
+        submenu.classList.add("hide");
+        if (icon) {
+            icon.innerHTML = "arrow_right";
+        }
+        }
+    } else { // 화살표가 아닌 다른 곳을 클릭하면 닫는다.
+        submenu.classList.add("hide");
+        if (icon) {
+        icon.innerHTML = "arrow_right";
+        }
+    }
+    });
+});
+}
